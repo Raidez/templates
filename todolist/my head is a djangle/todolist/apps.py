db2dc675abd0e44e66a3a75f099a925b3657d569
Alexis <alexis.lucien@outlook.fr>
2019-12-08 12:10:48 +0100

    Cleanage du code pour Django

--- todolist/my head is a djangle/todolist/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class TodolistConfig(AppConfig):
-    name = 'todolist'
--- todolist/my head is a djangle/todolist/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'todolist'
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('append/', views.add_tast, name="addtask"),
-    path('delete/<int:task_id>', views.delete_task, name="deltask")
-]
